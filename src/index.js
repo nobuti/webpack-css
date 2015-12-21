ab5c19a3c5178b23cdd0ee628bf83d0f43edd0ef
nobuti <buti@nobuti.com>
2015-12-21 09:30:12 +0100

    Example.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'stylesheets/base';
+import React from 'react';
+import Container from 'javascripts/container';
+React.render(<Container />, document.querySelector('#main'));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?includePaths[]=' + path.resolve(__dirname, './src')
+];
+
+const config = {
+  entry: {
+    app: ['./src/index']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      }
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './build'),
+    publicPath: '/build'
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss'],
+    modulesDirectories: ['src', 'node_modules']
+  }
+};
+
+module.exports = config;
